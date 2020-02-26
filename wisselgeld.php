f0341b0490d7b0821d1a76899862d7d985c7e4e1
Candace <2100978@talnet.nl>
2020-02-26 11:08:32 +0100

    First commit

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,33 +1,42 @@
 <?php
-
-$bedrag =  (float) round($argv[1], PHP_ROUND_HALF_UP);
-
-const GELDEENHEID = array(
-
-    "50 " => "Euro",
-    "20 " => "Euro",
-    "10 " => "Euro",
-    "5 " => "Euro",
-    "2 " => "Euro",
-    "1 " => "Euro",
-    "0.50 " => "Cent",
-    "0.25 " => "Cent",
-    "0.20 " => "Cent",
-    "0.10 " => "Cent",
-    "0.05 " => "Cent",
-    "0.5 " => "Cent",
-);
+$moneyChange =  (float) round($argv[1], 2, PHP_ROUND_HALF_UP);
 
 
-foreach (GELDEENHEID as $geld => $soort) {
-    $geld = (float) $geld;
-    $bedrag = round($bedrag, 2, PHP_ROUND_HALF_UP);
-    $count = floor($bedrag / $geld);
 
 
-    if (floor($bedrag / $geld)) {
-        $count =  floor($bedrag / $geld);
-        echo $count . " * $geld $soort" . PHP_EOL;
-        $bedrag = $bedrag -  round($geld, 2,);
+const GELDEENHEID = [
+    "50" => "Euro",
+    "20" => "Euro",
+    "10" => "Euro",
+    "5" => "Euro",
+    "2" => "Euro",
+    "1" => "Euro",
+    "0.50" => "Cent",
+    "0.20" => "Cent",
+    "0.10" => "Cent",
+    "0.05" => "Cent",
+    "0.5" => "Cent",
+    "0.2" => "Cent",
+    "0.1" => "Cent",
+    "0.05" => "Cent",
+    "0.02" => "Cent",
+    "0.01" => "Cent",
+];
+
+
+
+coinsBack(GELDEENHEID, $moneyChange);
+
+function coinsBack($geldeenheid, $moneyChange)
+{
+    foreach ($geldeenheid as $geld => $soort) {
+        $geld = (float) $geld;
+
+        if (floor($moneyChange / $geld) > 0) {
+            $count = floor($moneyChange / $geld);
+            echo "$count X $geld $soort" . PHP_EOL;
+            $moneyChange = $moneyChange -  ($count * $geld);
+            $moneyChange = round($moneyChange, 2);
+        }
     }
 }
